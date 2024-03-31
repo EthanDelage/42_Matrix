0c9e3e6a96dd78352465d47dc12d4398c25e801c
Ethan Delage <ethandelage04@gmail.com>
2024-03-31 01:59:09 +0100

    Add new tests for norm and angle_cos functions

--- src/tests/angle_cos.cc
+++ src/tests/angle_cos.cc
@@ -3,6 +3,7 @@
 #include <cassert>
 
 #include "./vector.h"
+#include "./tests.h"
 
 void angle_cos_tests() {
     Vector<float> v1({1., 0.});
@@ -14,4 +15,8 @@ void angle_cos_tests() {
     v1 = {2., 1.};
     v2 = {4., 2.};
     assert(1. == v1.angle_cos(v2));
+
+    Vector<float> v3({1., 2., 3.});
+    Vector<float> v4({4., 5., 6.});
+    assert_float(0.974631846, v3.angle_cos(v4));
 }
--- src/tests/norm.cc
+++ src/tests/norm.cc
@@ -3,6 +3,7 @@
 #include <cassert>
 
 #include "./vector.h"
+#include "./tests.h"
 
 void vector_norm_tests() {
     Vector<float> v1({0., 0., 0.});
@@ -12,11 +13,11 @@ void vector_norm_tests() {
 
     v1 = {1., 2., 3.};
     assert(6. == v1.taxicab_norm());
-    assert(std::sqrtf(14.) == v1.euclidean_norm());
+    assert_float(3.74165738, v1.euclidean_norm());
     assert(3. == v1.uniform_norm());
 
     Vector<float> v2({-1., -2.});
     assert(3. == v2.taxicab_norm());
-    assert(std::sqrtf(5.) == v2.euclidean_norm());
+    assert_float(2.236067977, v2.euclidean_norm());
     assert(2. == v2.uniform_norm());
 }
